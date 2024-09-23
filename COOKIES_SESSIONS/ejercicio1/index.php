3e7911b421ada9089e82a675d4821b7f2057b479
DW32 TALDEA <ik012982i10@H012982.NET>
2024-09-23 13:40:23 +0200

    cookie_ej1

---
+++ COOKIES_SESSIONS/ejercicio1/index.php
@@ -0,0 +1,43 @@
+<?php
+    $cookie_name = "visitas";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST['boton_borrar'])) {
+            setcookie($cookie_name, "", time() - 3600);
+            header("Refresh:0");
+        }
+    } else {
+        if (!isset($_COOKIE[$cookie_name])) {
+            $cookie_value = 0;
+        } else {
+            $cookie_value = $_COOKIE[$cookie_name];
+            $cookie_value++;
+        }
+    
+        setcookie($cookie_name, $cookie_value);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="POST" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>>
+        <input type="submit" value="Borrar cookie" name="boton_borrar">
+    </form>
+    <?php
+    if ($cookie_value == 0) {
+        echo "Es tu primera visita";
+    } else {
+        echo "Es tu visita número " . $cookie_value;
+    }
+    ?>
+</body>
+
+</html>
